4bc359f8ecc13c8419f99ac7ff04cb0b66d2e797
JetBlackCodes <melnikova_ia@astral.ru>
2022-05-26 11:57:24 +0300

    add mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CheckboxesList from './components/CheckboxesList';
+
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+import { Provider } from 'mobx-react';
+import TodoStore from './stores/TodoStore';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider TodoStore={TodoStore}>
+        <CheckboxesList />
+      {/*<div className="App">*/}
+      {/*  <FormGroup>*/}
+      {/*    <FormControlLabel control={<Checkbox />} label="Забрать заказ с ozon" />*/}
+      {/*    <FormControlLabel disabled control={<Checkbox />} label="Написать приложение с использованием mobX" />*/}
+      {/*    <FormControlLabel disabled control={<Checkbox />} label="Полить цветочки" />*/}
+      {/*    <FormControlLabel disabled control={<Checkbox />} label="Пропылесосить квартиру" />*/}
+      {/*  </FormGroup>*/}
+      {/*</div>*/}
+    </Provider>
   );
 }
 
---
+++ src/stores/TodoStore.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { observable, action, computed } from 'mobx';
+
+class TodoStore {}
+
+const store = new TodoStore();
+export default store;
